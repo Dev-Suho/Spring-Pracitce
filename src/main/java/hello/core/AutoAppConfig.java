04ecf146ca48e44ad6bd8011c7c7e81a0f714dcb
Dev-Suho <devsuho99@gmail.com>
2023-03-09 11:02:07 +0900

    0309 autowired pratice

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        basePackages = "hello.core.member", // 디폴트는 프로젝트 시작 루트 hello.core, 프로젝트 최상단에 두는 것을 권장
+        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
+public class AutoAppConfig {
+}
---
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -0,0 +1,34 @@
+package hello.core.singleton;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatefulServiceTest {
+
+    @Test
+    void statefulServiceSingleton() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(TestConfig.class);
+        StatefulService bean1 = ac.getBean(StatefulService.class);
+        StatefulService bean2 = ac.getBean(StatefulService.class);
+
+        bean1.order("userA", 10000);
+        bean2.order("userB", 20000);
+
+        int price = bean1.getPrice();
+        System.out.println("price = " + price);
+
+        Assertions.assertThat(bean1.getPrice()).isEqualTo(20000);
+    }
+
+    static class TestConfig {
+        @Bean
+        public StatefulService statefulService() {
+            return new StatefulService();
+        }
+    }
+}
